e8e4e2f5e9d25d6a112b20d18e76ddd5894bc771
Dragon4396 <hekunlong2515@gmail.com>
2024-09-05 21:34:03 -0700

    add react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,22 @@
 import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom' 
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HomeScreen from './screens/HomeScreen';
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <HomeScreen />
+          <Routes>
+            <Route path='/' element={<HomeScreen />} exact/>
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
