b811c5119121e93148a5cae27be3c8b9d1377870
Dragon4396 <hekunlong2515@gmail.com>
2024-09-09 15:11:48 -0700

    set env

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const users = require('./data/users')
+const dotenv = require('dotenv')
 
+dotenv.config()
 const app = express()
 
 app.get('/', (req, res) => {
@@ -11,4 +13,6 @@ app.get('/api/users', (req, res) => {
     res.json(users)
 })
 
-app.listen(5000, console.log("The server is running on port 5000"))
+const PORT = process.env.PORT | 5000
+
+app.listen(PORT, console.log(`The server is running on port ${PORT} under ${process.env.NODE_ENV} mode`))
